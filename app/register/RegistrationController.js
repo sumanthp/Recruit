37e70441e97497e8d5c9b44483bbef891004e4f9
unknown <sumanth472@gmail.com>
2018-12-09 17:58:28 +0530

    Login Register changes to post to apiservices

--- app/register/RegistrationController.js
+++ app/register/RegistrationController.js
@@ -1,9 +1,23 @@
 angular.module('recruitApp.RegistrationController',['recruitApp.Authentication'])
-    .controller('RegistrationController', ['$scope', 'Authentication', function ($scope, Authentication){
+    .controller('RegistrationController', ['$scope', '$http', 'Authentication', '$httpParamSerializerJQLike', '$state', function ($scope, $http, Authentication, $httpParamSerializerJQLike, $state){
         "use strict";
         $scope.message="Register with your Email Id";
         $scope.register = function () {
-            Authentication.register($scope.user);
+            //Authentication.register($scope.user);
+            $http({
+                method:"POST", 
+                url:"https://recruit-apiservices.herokuapp.com/api/register", 
+                data:$httpParamSerializerJQLike($scope.user),
+                headers:{'Content-Type':'application/x-www-form-urlencoded; charset=utf-8'}
+            }).success(function(data, status, headers, config){
+                if(data.message){
+                    $scope.success = data.message;
+                    $state.go('recruit')
+                }
+            }).error(function(data, status, headers, config){
+                if(data.message){
+                    $scope.success = data.message;
+                }
+            });
         };
-
 }]);
