4ae6cd51ee0acaecc3b16b580d313c87af697efe
polisetty sumanth <sumanth472@gmail.com>
2019-05-20 23:21:14 +0530

    Profile Page client changes

--- app/profile/ProfileController.js
+++ app/profile/ProfileController.js
@@ -5,9 +5,17 @@
         .module('recruitApp')
         .controller('ProfileController', ProfileController);
 
-    ProfileController.$inject = ['$state', 'AuthenticationService', '$scope'];
-    function ProfileController($state, AuthenticationService, $scope) {
-
+    ProfileController.$inject = ['$state', 'AuthenticationService', '$scope', 'angularFileUpload'];
+    function ProfileController($state, AuthenticationService, $scope, FileUploader) {
+        $scope.registrants = ["Student", "Recruiter"];
+        var vm = this;
+        $scope.user = {name:"hemanth", isJob:"true", option:"yes", designation:"student", email:"hemanthss333@gmail.com", bio:"technology Enthusiast", contact:"9738213033", college:"PESIT"};
+        $scope.content={about:"technically skilled to problem solving", skills:"html, css, java, unix, ml", roles:"software engineer1"};
+        $scope.editProfile = edit;
+        function edit()
+        {
+            $state.go('editProfile');
+        }
     }
 
 })();
