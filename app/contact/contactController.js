171d26e79db761c66624d0498e4c5ff307032906
polisetty sumanth <sumanth472@gmail.com>
2019-05-25 13:09:44 +0530

    Contact changes

--- app/contact/contactController.js
+++ app/contact/contactController.js
@@ -7,16 +7,7 @@
   
         contactController.$inject = ['$location', '$scope', '$state'];
     function contactController($location, $scope, $state) {
-        $scope.registrants = ["Student", "Recruiter"];
-        //envService.set('development');
-        var vm = this;
-        $scope.user = {name:"hemanth", isJob:"true", option:"yes", designation:"student", email:"hemanthss333@gmail.com", bio:"technology Enthusiast", contact:"9738213033", college:"PESIT"};
-        $scope.content={about:"technically skilled to problem solving", skills:"html, css, java, unix, ml", roles:"software engineer1"};
-        $scope.editProfile = edit;
-        function edit()
-        {
-            $state.go('editProfile');
-        }
+        
     }
   
   })();
